95009794961b396820bf6866541a427b2fc6a7f4
Tomek <tomasz@tomaka.eu>
2017-02-26 20:07:15 +0100

    Change configuration to use Injection

--- src/main/java/eu/tomaka/repo/HibernateCustomerRepoImpl.java
+++ src/main/java/eu/tomaka/repo/HibernateCustomerRepoImpl.java
@@ -3,18 +3,16 @@ package eu.tomaka.repo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import eu.tomaka.module.Customer;
 
 public class HibernateCustomerRepoImpl implements CustomerRepo {
-
+	@Value("${username}")
 	private String username;
+	@Value("${password}")
 	private String password;
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
 	/* (non-Javadoc)
 	 * @see eu.tomaka.repo.CustomerRepo#findAll()
 	 */
