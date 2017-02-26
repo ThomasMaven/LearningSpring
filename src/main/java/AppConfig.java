613308de242f9f84b6974a3460695fa59fea539a
Tomek <tomasz@tomaka.eu>
2017-02-26 20:25:34 +0100

    use properties in Java

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,8 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import eu.tomaka.repo.CustomerRepo;
 import eu.tomaka.repo.HibernateCustomerRepoImpl;
@@ -9,7 +11,12 @@ import eu.tomaka.service.CustomerServiceImpl;
 
 @Configuration
 @ComponentScan({"eu.tomaka"})
+@PropertySource("app.properties")
 public class AppConfig {
-	
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 
 }
--- src/main/java/eu/tomaka/repo/HibernateCustomerRepoImpl.java
+++ src/main/java/eu/tomaka/repo/HibernateCustomerRepoImpl.java
@@ -3,6 +3,7 @@ package eu.tomaka.repo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import eu.tomaka.module.Customer;
@@ -10,11 +11,14 @@ import eu.tomaka.module.Customer;
 @Repository("customerRepository")
 public class HibernateCustomerRepoImpl implements CustomerRepo {
 
+	@Value("${someVariable}")
+	private String someVariable;
 	/* (non-Javadoc)
 	 * @see eu.tomaka.repo.CustomerRepo#findAll()
 	 */
 	@Override
 	public List<Customer> findAll() {
+		System.out.println("Some value from config is \"" + someVariable + "\"");
 		List<Customer> customers = new ArrayList<>();
 		Customer customer = new Customer();
 		customer.setName("Jan");
